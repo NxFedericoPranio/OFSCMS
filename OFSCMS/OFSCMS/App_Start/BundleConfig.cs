d0bf78dc196c1d36a249f3195335642a87602d2d
Federico Pranio <federico.pranio@nexusat.it>
2014-09-03 12:14:54 +0200

    bundle

---
+++ OFSCMS/OFSCMS/App_Start/BundleConfig.cs
@@ -0,0 +1,62 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace OFSCMS
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/framework/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/framework/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/framework/jquery.unobtrusive*",
+                        "~/Scripts/framework/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/framework/angular.js"
+                        , "~/Scripts/framework/angular-route.js"
+                        , "~/Scripts/framework/angular-resource.js"
+                        , "~/Scripts/framework/angular-animate.js"
+                        , "~/Scripts/framework/angular-sanitize.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(                
+                        "~/Scripts/JqueryScript.js"
+                        ,"~/Scripts/JquerySlides.js"
+                        ,"~/Scripts/Controllers/ofs.controllers.js"
+                        ,"~/Scripts/Controllers/configurations.js"
+                        ,"~/Scripts/Controllers/photoAlbumModule.js"                
+                ));
+
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/Site.css"
+                ,"~/Content/Animate.css"
+                ));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Content/themes/base/jquery.ui.button.css",
+                        "~/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Content/themes/base/jquery.ui.slider.css",
+                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Content/themes/base/jquery.ui.theme.css"));
+        }
+    }
+}
--- OFSCMS/OFSCMS/Global.asax.cs
+++ OFSCMS/OFSCMS/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace OFSCMS
@@ -24,6 +25,8 @@ namespace OFSCMS
             //RouteConfig.RegisterRoutes(RouteTable.Routes);
 
             AreaRegistration.RegisterAllAreas();
+
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             //// Use LocalDB for Entity Framework by default
             //Database.DefaultConnectionFactory = new SqlConnectionFactory("Data Source=(localdb)\v11.0; Integrated Security=True; MultipleActiveResultSets=True");
